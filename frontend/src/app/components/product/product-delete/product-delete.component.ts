8a939238212a2fda4b89a09658e455cd9d3da2a9
Felipe Bernardo <ffeh.bernardo@gmail.com>
2021-02-11 10:32:48 -0300

    Tela de Produto

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductDeleteComponent implements OnInit {
 
-  product: Product
+  product: Product | undefined
 
   constructor(
     private productService: ProductService, 
